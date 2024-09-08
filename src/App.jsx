1a816f7b77a05062218bba146ca6180b9616b763
beatrizbb99 <beatrizbarrosbarz@gmail.com>
2024-09-08 21:56:13 +0200

    aaa

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";  
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import axios from "axios";
 axios.defaults.withCredentials = true;
@@ -11,12 +11,11 @@ import Logout from "./components/Logout";
 import Login from "./components/Login";
 import CircularLoadingBar from "./components/CircularLoadingBar";
 
-const PORT = 3001;
-
 const GameScreen = () => {
   const [authUserId, setAuthUserId] = useState("");
   const [userId, setUserId] = useState("");
   const [name, setName] = useState("");
+  const navigate = useNavigate();
 
   const checkGame = async () => {
     try {
@@ -24,25 +23,19 @@ const GameScreen = () => {
         `https://adventure-clicker-backend.onrender.com`
       );
 
-      // If the response status is 200, it means the user is authenticated
+      // If the response is successful, the user is authenticated
       console.log("Authentication response:", response);
       setAuthUserId(response.data.user._id);
       toast.success(`Angemeldet als ${response.data.user.username}`);
     } catch (error) {
-      // Check if it's a redirect
-      toast.info(error);
-      if (
-        error.response &&
-        error.response.status === 302 &&
-        error.response.headers.location
-      ) {
-        const redirectUrl = error.response.headers.location;
-        console.log("Redirecting to:", redirectUrl);
+      // Check if it's a 401 Unauthorized error
+      if (error.response && error.response.status === 401) {
+        toast.warn("Nicht authentifiziert, Weiterleitung zur Login-Seite.");
         // Redirect to the login page
-        toast.warn("Redirect 1");
+        navigate("/login");
       } else {
         console.error("Login failed:", error);
-        toast.error(`Login fehlgeschlagen ${error}`);
+        toast.error(`Login fehlgeschlagen ${error.message}`);
       }
     }
   };
@@ -56,32 +49,22 @@ const GameScreen = () => {
       try {
         const response = await axios.post(`https://adventure-clicker-backend.onrender.com/graphql`, {
           query: `
-                query {
-                    getUserByAuth(authUserId:"${authUserId}"){
-                        id
-                        name
-                    }
+            query {
+                getUserByAuth(authUserId:"${authUserId}"){
+                    id
+                    name
                 }
-              `,
+            }
+          `,
         },
-          { withCredentials: true }
-        );
+        { withCredentials: true });
 
-        // If the response status is 200, it means the user is authenticated
-        //console.log("getUserId response:", response.data.data.getUserByAuth.id);
         setUserId(response.data.data.getUserByAuth.id);
         setName(response.data.data.getUserByAuth.name);
       } catch (error) {
-        // Check if it's a redirect
-        if (
-          error.response &&
-          error.response.status === 302 &&
-          error.response.headers.location
-        ) {
-          const redirectUrl = error.response.headers.location;
-          console.log("Redirecting to:", redirectUrl);
-          // Redirect to the login page
-          toast.warn("Redirect 2");
+        if (error.response && error.response.status === 401) {
+          toast.warn("Nicht authentifiziert, Weiterleitung zur Login-Seite.");
+          navigate("/login");
         } else {
           console.error("Login failed:", error);
           toast.error("Login fehlgeschlagen 2");
@@ -93,7 +76,7 @@ const GameScreen = () => {
       console.log("authUserId", authUserId);
       getUserId();
     }
-  }, [authUserId]);
+  }, [authUserId, navigate]);
 
   return (
     <div>
